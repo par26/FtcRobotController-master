3ab58aa6ccb720f1c80ed22100f186455bdc1891
rocketmind26 <apratush26@gmail.com>
2023-10-14 14:30:48 -0700

    Added RoadRunner to project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -2,5 +2,30 @@ package org.firstinspires.ftc.teamcode;
 
 public class Arm {
 
+    int num;
 
+
+    public void extendClaw() {
+        num = 5;
+    }
+
+    public void retractClaw() {
+
+    }
+
+    public void openClaw() {
+
+    }
+
+    public void closeClaw() {
+
+    }
+
+    public void bendUp() {
+
+    }
+
+    public void bendDown() {
+
+    }
 }
