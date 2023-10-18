584cbffd5645343c08a0ef018204c627435e0a2a
Crympty <CrimsonCrympty@gmail.com>
2023-10-17 23:32:19 -0700

    Commit to show changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -1,23 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
-
-
-
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Arm {
-        double leftOpenPositionValue; //declaring servo's open position value so easily manipulable
-        double leftClosePositionValue; //declaring servo's close position value so easily manipulable
-        double rightOpenPositionValue; //declaring servo's open position value so easily manipulable
-        double rightClosePositionValue; //declaring servo's close position value so easily manipulable
+        double leftOpenPositionValue = 0.0; //declaring servo's open position value so easily manipulable
+        double leftClosePositionValue = 0.0; //declaring servo's close position value so easily manipulable
+        double rightOpenPositionValue = 0.0; //declaring servo's open position value so easily manipulable
+        double rightClosePositionValue = 0.0; //declaring servo's close position value so easily manipulable
 
-        Servo leftClawMotor;
-        Servo rightClawMotor;
+        Servo leftClawServo;
+        Servo rightClawServo;
 
         public void init(HardwareMap hwMap) {
-                rightClawMotor = hwMap.get(Servo.class, "leftClaw");
-                leftClawMotor = hwMap.get(Servo.class, "leftClaw");
+                rightClawServo = hwMap.get(Servo.class, "rightClaw");
+                leftClawServo = hwMap.get(Servo.class, "leftClaw");
         }
 
         public boolean isButtonPressedChecker(boolean gamePad) {
@@ -29,13 +26,60 @@ public class Arm {
                 isPressed = gamePad;
                 return lastPressed;
         }
-        public void closeClaw() {
-                leftClawMotor.setPosition(leftClosePositionValue);
-                rightClawMotor.setPosition(rightClosePositionValue);
+
+        public void openClawCL() { //bang bang
+                double currentLCPosition = leftClawServo.getPosition();
+                double currentRCPosition = rightClawServo.getPosition();
+
+                while (currentLCPosition != leftOpenPositionValue || rightOpenPositionValue != rightOpenPositionValue) {
+
+                        double unroundedLCError = leftOpenPositionValue - currentLCPosition;
+                        double LCError = Math.round(unroundedLCError * 10.0) / 10.0;
+
+                        if (LCError > 0) { //will exit when RCError = 0
+                                leftClawServo.setPosition(-0.1);
+                        } else if (LCError != 0) {
+                                leftClawServo.setPosition(+0.1);
+                        }
+
+
+                        double unroundedRCError = rightOpenPositionValue - currentRCPosition;
+                        double RCError = Math.round(unroundedRCError * 10.0) / 10.0;
+
+
+                        if (RCError > 0) { // will exit when RCError = 0
+                                rightClawServo.setPosition(-0.1); //questions for big P tomorrow!!!
+                        } else if (RCError != 0) {
+                                rightClawServo.setPosition(+0.1);
+                        }
+                }
+
         }
-        public void openClaw() {
-                leftClawMotor.setPosition(leftOpenPositionValue);
-                rightClawMotor.setPosition(rightOpenPositionValue);
+        /* public void openClawCL() { // proportional
+                double currentLCPosition = leftClawServo.getPosition();
+                double currentRCPosition = rightClawServo.getPosition();
+
+                while (currentLCPosition != leftOpenPositionValue || rightOpenPositionValue != rightOpenPositionValue) {
+
+                        double unroundedLCError = leftOpenPositionValue - currentLCPosition;
+                        double LCError = Math.round(unroundedLCError * 10.0) / 10.0;
+
+                        leftClawServo.setPosition(LCError);
+
+                        double unroundedRCError = rightOpenPositionValue - currentRCPosition;
+                        double RCError = Math.round(unroundedRCError * 10.0) / 10.0; */ //doubt this works since not loop :troll:
+
+
+
+
+
+        public void closeClaw() { // ignore this
+                leftClawServo.setPosition(leftClosePositionValue);
+                rightClawServo.setPosition(rightClosePositionValue);
+        }
+        public void openClaw() { // ignore this
+                leftClawServo.setPosition(leftOpenPositionValue);
+                rightClawServo.setPosition(rightOpenPositionValue);
         }
 
 
