57775c6f516f8efdc2f5f4039967c6f69d52a796
rocketmind26 <apratush26@gmail.com>
2023-10-25 17:48:28 -0700

    Changes the open and close claws to set position functions because it has built in PID :(

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/DetectPixel.java
@@ -0,0 +1,136 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetectPixel implements VisionProcessor {
+
+    Mat submat = new Mat();
+    Mat hsvMat = new Mat();
+
+    Mat thresh = new Mat();
+
+
+    //backlog of frames to average out to reduce noise
+    ArrayList<double[]> frameList;
+    //these are public static to be tuned in dashboard
+    public static double strictLowS = 140;
+    public static double strictHighS = 255;
+
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+        frameList = new ArrayList<>();
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        Mat mat = new Mat();
+
+        //mat turns into HSV value
+        Imgproc.cvtColor(frame, mat, Imgproc.COLOR_RGB2HSV);
+        if (mat.empty()) {
+            return frame;
+        }
+
+        // lenient bounds will filter out near yellow, this should filter out all near yellow things(tune this if needed)
+        Scalar lowHSV = new Scalar(20, 70, 80); // lenient lower bound HSV for yellow
+        Scalar highHSV = new Scalar(0, 0, 255); // lenient higher bound HSV for yellow
+
+        Mat thresh = new Mat();
+
+        // Get a black and white image of yellow objects
+        Core.inRange(mat, lowHSV, highHSV, thresh);
+
+        Mat masked = new Mat();
+        //color the white portion of thresh in with HSV from mat
+        //output into masked
+        Core.bitwise_and(mat, mat, masked, thresh);
+        //calculate average HSV values of the white thresh values
+        Scalar average = Core.mean(masked, thresh);
+
+        Mat scaledMask = new Mat();
+        //scale the average saturation to 150
+        masked.convertTo(scaledMask, -1, 150 / average.val[1], 0);
+
+
+        Mat scaledThresh = new Mat();
+        //you probably want to tune this
+        Scalar strictLowHSV = new Scalar(0, strictLowS, 0); //strict lower bound HSV for yellow
+        Scalar strictHighHSV = new Scalar(255, strictHighS, 255); //strict higher bound HSV for yellow
+        //apply strict HSV filter onto scaledMask to get rid of any yellow other than pole
+        Core.inRange(scaledMask, strictLowHSV, strictHighHSV, scaledThresh);
+
+        Mat finalMask = new Mat();
+        //color in scaledThresh with HSV, output into finalMask(only useful for showing result)(you can delete)
+        Core.bitwise_and(mat, mat, finalMask, scaledThresh);
+
+        Mat edges = new Mat();
+        //detect edges(only useful for showing result)(you can delete)
+        Imgproc.Canny(scaledThresh, edges, 100, 200);
+
+        //contours, apply post processing to information
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        //find contours, frame scaledThresh because it has hard edges
+        Imgproc.findContours(scaledThresh, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        //list of frames to reduce inconsistency, not too many so that it is still real-time, change the number from 5 if you want
+        if (frameList.size() > 5) {
+            frameList.remove(0);
+        }
+
+
+        //release all the data
+        frame.release();
+        scaledThresh.copyTo(frame);
+        scaledThresh.release();
+        scaledMask.release();
+        mat.release();
+        masked.release();
+        edges.release();
+        thresh.release();
+        finalMask.release();
+        //change the return to whatever mat you want
+        //for example, if I want to look at the lenient thresh:
+        // return thresh;
+        // note that you must not do thresh.release() if you want to return thresh
+        // you also need to release the frame if you return thresh(release as much as possible)
+        return frame;
+    }
+
+
+    private android.graphics.Rect makeGraphicsRect(Rect rect, float scaleBmpPxToCanvasPx) {
+        int left = Math.round(rect.x * scaleBmpPxToCanvasPx);
+        int top = Math.round(rect.y * scaleBmpPxToCanvasPx);
+        int right = left + Math.round(rect.width * scaleBmpPxToCanvasPx);
+        int bottom = top + Math.round(rect.height * scaleBmpPxToCanvasPx);
+
+        return new android.graphics.Rect(left, top, right, bottom);
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        Rect rect = new Rect(20, 20, 50, 50);
+
+        Paint rectPaint = new Paint();
+        rectPaint.setColor(Color.RED);
+        rectPaint.setStyle(Paint.Style.STROKE);
+        rectPaint.setStrokeWidth(scaleCanvasDensity * 4);
+
+        canvas.drawRect(makeGraphicsRect(rect, scaleBmpPxToCanvasPx), rectPaint);
+    }
+}
